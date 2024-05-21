979523bc47c3582e14c99f7d7f2f2a7ec7d386a8
codeedexpython <codeedextechnologies@gmail.com>
2024-05-21 15:27:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Aboutus from './pages/Aboutus';
+import Contactus from './pages/Contactus';
+import Services from './pages/Services';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Routes>
+      <Route path='/' element={<Home></Home>} ></Route>
+      <Route path='/about' element={<Aboutus></Aboutus>} ></Route>
+      <Route path='/contactus' element={<Contactus></Contactus>} ></Route>
+      <Route path='/services' element={<Services></Services>} ></Route>
+      <Route path='/footer' element={<Footer></Footer>} ></Route>
+
+
+
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
